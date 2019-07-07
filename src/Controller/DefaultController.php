23da7d81c55900403e39be8c85b787769db7867c
ABGEO <takalandzet@gmail.com>
2019-07-08 01:36:15 +0400

    Add contact page.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType;
+use App\Service\MailerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class DefaultController extends AbstractController
 {
@@ -18,6 +24,48 @@ class DefaultController extends AbstractController
     public function index()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * Contact Action.
+     *
+     * @Route("/contact", name="contact")
+     *
+     * @param Request $request
+     * @param MailerService $mailer
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function contact(Request $request, MailerService $mailer)
+    {
+        $form = $this->createForm(ContactFormType::class);
+
+        $form->handleRequest($request);
+
+        // Send email
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            $sendStatus = false;
+            try {
+                $sendStatus = $mailer->sendMessage(
+                    [getenv('CONTACT_EMAIL')],
+                    $formData['subject'] . ' | abgeo.dev',
+                    'email/contact.html.twig',
+                    $formData,
+                    'no-replay@abgeo.dev'
+                );
+            } catch (LoaderError $e) {
+            } catch (RuntimeError $e) {
+            } catch (SyntaxError $e) {
+            }
+
+            $this->addFlash( 'contact-email-status', $sendStatus ? 'success' : 'error');
+        }
+
+        return $this->render('default/contact.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
 }
---
+++ src/Form/ContactFormType.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ContactFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('full_name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter Your Name'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'validation.is_blank'
+                    ])
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter E-Mail address'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'validation.is_blank'
+                    ]),
+                    new Email([
+                        'message' => 'validation.invalid_email'
+                    ])
+                ]
+            ])
+            ->add('subject', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter Subject'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'validation.is_blank'
+                    ])
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter Message'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'validation.is_blank'
+                    ])
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
