46d4a146ddc97977873e663fbf468a533a671ebd
Temuri Takalandze <takalandzet@gmail.com>
2021-11-28 15:23:35 +0400

    Update homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -32,129 +32,43 @@ class DefaultController extends AbstractController
     {
         $recentProjects = $repository->getSortedProjects(5);
         $skills = [
-            [
-                'title' => 'Algorithms',
-                'icon' => 'fa-object-ungroup',
-            ],
-            [
-                'title' => 'Data Structures',
-                'icon' => 'fa-table',
-            ],
-            [
-                'title' => 'OOP',
-                'icon' => 'fa-tags',
-            ],
-            [
-                'title' => 'Web Development',
-                'icon' => 'fa-code',
-            ],
-            [
-                'title' => 'Testing',
-                'icon' => 'fa-bug',
-            ],
-            [
-                'title' => 'Unit Testing',
-                'icon' => 'fa-bug',
-            ],
-            [
-                'title' => 'PHPUnit',
-                'icon' => 'fa-bug',
-            ],
-            [
-                'title' => 'Automation',
-                'icon' => 'fa-magic',
-            ],
-            [
-                'title' => 'Programming',
-                'icon' => 'fa-code',
-            ],
-            [
-                'title' => 'Linux',
-                'icon' => 'fa-linux',
-            ],
-            [
-                'title' => 'Jenkins',
-                'icon' => 'fa-tags',
-            ],
-            [
-                'title' => 'Travis CI',
-                'icon' => 'fa-tags',
-            ],
-            [
-                'title' => 'CircleCI',
-                'icon' => 'fa-circle-o-notch',
-            ],
-            [
-                'title' => 'Git',
-                'icon' => 'fa-git',
-            ],
-            [
-                'title' => 'PHP',
-                'icon' => 'fa-code',
-            ],
-            [
-                'title' => 'SQL',
-                'icon' => 'fa-database',
-            ],
-            [
-                'title' => 'MySQL',
-                'icon' => 'fa-database',
-            ],
-            [
-                'title' => 'JavaScript',
-                'icon' => 'fa-code',
-            ],
-            [
-                'title' => 'jQuery',
-                'icon' => 'fa-tags',
-            ],
-            [
-                'title' => 'Java',
-                'icon' => 'fa-code',
-            ],
-            [
-                'title' => 'Github',
-                'icon' => 'fa-github',
-            ],
-            [
-                'title' => 'Arduino',
-                'icon' => 'fa-code',
-            ],
-            [
-                'title' => 'Python',
-                'icon' => 'fa-code',
-            ],
-            [
-                'title' => 'Jira',
-                'icon' => 'fa-tags',
-            ],
-            [
-                'title' => 'Trello',
-                'icon' => 'fa-trello',
-            ],
-            [
-                'title' => 'JSON',
-                'icon' => 'fa-tags',
-            ],
-            [
-                'title' => 'HTML',
-                'icon' => 'fa-html5',
-            ],
-            [
-                'title' => 'Defensive Programming',
-                'icon' => 'fa-code',
-            ],
-            [
-                'title' => 'Back-End Web Development',
-                'icon' => 'fa-code',
-            ],
-            [
-                'title' => 'Github Actions',
-                'icon' => 'fa-github-alt',
-            ],
+            ['title' => 'Algorithms', 'icon' => 'fa-object-ungroup'],
+            ['title' => 'Data Structures', 'icon' => 'fa-table'],
+            ['title' => 'OOP', 'icon' => 'fa-tags'],
+            ['title' => 'Web Development', 'icon' => 'fa-code'],
+            ['title' => 'Testing', 'icon' => 'fa-bug'],
+            ['title' => 'Unit Testing', 'icon' => 'fa-bug'],
+            ['title' => 'PHPUnit', 'icon' => 'fa-bug'],
+            ['title' => 'Automation', 'icon' => 'fa-magic'],
+            ['title' => 'Programming', 'icon' => 'fa-code'],
+            ['title' => 'Linux', 'icon' => 'fa-linux'],
+            ['title' => 'Docker', 'icon' => 'fa-tags'],
+            ['title' => 'Ansible', 'icon' => 'fa-tags'],
+            ['title' => 'Terraform', 'icon' => 'fa-tags'],
+            ['title' => 'Traefik', 'icon' => 'fa-tags'],
+            ['title' => 'NGINX', 'icon' => 'fa-tags'],
+            ['title' => 'Apache', 'icon' => 'fa-tags'],
+            ['title' => 'Jenkins', 'icon' => 'fa-tags'],
+            ['title' => 'Travis CI', 'icon' => 'fa-tags'],
+            ['title' => 'CircleCI', 'icon' => 'fa-circle-o-notch'],
+            ['title' => 'Git', 'icon' => 'fa-git'],
+            ['title' => 'PHP', 'icon' => 'fa-code'],
+            ['title' => 'SQL', 'icon' => 'fa-database'],
+            ['title' => 'MySQL', 'icon' => 'fa-database'],
+            ['title' => 'JavaScript', 'icon' => 'fa-code'],
+            ['title' => 'jQuery', 'icon' => 'fa-tags'],
+            ['title' => 'Java', 'icon' => 'fa-code'],
+            ['title' => 'Github', 'icon' => 'fa-github'],
+            ['title' => 'Arduino', 'icon' => 'fa-code'],
+            ['title' => 'Python', 'icon' => 'fa-code'],
+            ['title' => 'Jira', 'icon' => 'fa-tags'],
+            ['title' => 'Trello', 'icon' => 'fa-trello'],
+            ['title' => 'JSON', 'icon' => 'fa-tags'],
+            ['title' => 'HTML', 'icon' => 'fa-html5'],
+            ['title' => 'Defensive Programming', 'icon' => 'fa-code'],
+            ['title' => 'Back-End Web Development', 'icon' => 'fa-code'],
+            ['title' => 'Github Actions', 'icon' => 'fa-github-alt'],
         ];
-
-        shuffle($skills);
 
         return $this->render('default/index.html.twig', [
             'recentProjects' => $recentProjects,
